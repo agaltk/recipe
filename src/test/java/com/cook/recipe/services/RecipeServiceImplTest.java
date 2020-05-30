0c98a92e10b24525b66f77a7e02b0f132a94fc51
Aga <aga.ltk@gmail.com>
2020-05-30 23:00:27 +0200

    show recipe added

--- src/test/java/com/cook/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/com/cook/recipe/services/RecipeServiceImplTest.java
@@ -9,9 +9,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 class RecipeServiceImplTest {
@@ -28,6 +31,23 @@ class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipesByTestId() throws Exception{
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe returnedRecipe = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", returnedRecipe);
+        verify(recipeRepository,times(1)).findById(anyLong());
+    }
+    @Test
     public void getRecipes() throws Exception{
 
         Recipe recipe = new Recipe();
@@ -40,6 +60,7 @@ class RecipeServiceImplTest {
         assertEquals(recipes.size(), 1);
 
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository,never()).findById(anyLong());
     }
 
 }
