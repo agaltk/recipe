0c4247ff39768a506c7372672c66bbe4392b9ad2
Aga <aga.ltk@gmail.com>
2020-06-01 14:30:42 +0200

    add recipe

--- src/main/java/com/cook/recipe/controllers/RecipeController.java
+++ src/main/java/com/cook/recipe/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package com.cook.recipe.controllers;
 
+import com.cook.recipe.commands.RecipeCommand;
 import com.cook.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -23,6 +23,20 @@ public class RecipeController {
         return "recipe/show";
 
 
+    }
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command)
+    {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show" + savedCommand.getId();
     }
 
 }
